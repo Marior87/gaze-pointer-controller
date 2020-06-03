06cc85bc599e435b36ceb7d1d234c9c0a74ce023
Marior87 <mariorivas87@gmail.com>
2020-06-03 11:40:19 -0300

    Completed models handler class

--- src/model.py
+++ src/model.py
@@ -25,6 +25,7 @@ class GenericModel:
         self.out_blob = None
         self.net_plugin = None
         self.infer_request_handle = None
+        self.outputs = None
 
 
     def load_model(self, model, cpu_extension=None, plugin=None):
@@ -73,7 +74,7 @@ class GenericModel:
         '''
         preprocessed_image = self.preprocess_input(image)
         self.infer_request_handle = self.net_plugin.start_async(request_id=request_id, 
-                                                                inputs={'image_tensor': preprocessed_image})
+                                                                inputs={self.input_blob: preprocessed_image})
 
         return self.net_plugin
 
@@ -86,7 +87,7 @@ class GenericModel:
         '''
 
         input_shape = self.net.inputs[self.input_blob].shape
-        preprocessed_image = utils.handle_image(image, input_shape[1], input_shape[0])
+        preprocessed_image = utils.handle_image(image, input_shape[3], input_shape[2])
 
         return preprocessed_image
 
@@ -97,7 +98,9 @@ class GenericModel:
 
     def get_output(self, request_id=0):
 
-        return self.net_plugin.requests[request_id].outputs[self.out_blob]
+        self.outputs = self.net_plugin.requests[request_id].outputs[self.out_blob]
+
+        return self.outputs
 
 
 class FaceDetector(GenericModel):
@@ -112,5 +115,67 @@ class FaceDetector(GenericModel):
         '''
 
         return self.outputs[0,0]
-        
-        
+
+class HeadPose(GenericModel):
+
+    def __init__(self, model_path=HEAD_POSE_MODEL):
+        super().__init__()
+        self.load_model(model_path)
+
+    def get_output(self, request_id=0):
+        '''
+        Por ahora devolviendo un diccionario, depende de como se vaya a manejar la lógica después se modifica preprocess_output.
+        '''
+        self.outputs = self.net_plugin.requests[request_id].outputs
+
+        return self.outputs
+
+    
+    def preprocess_output(self):
+        '''
+        Output layer names in Inference Engine format:
+
+    name: "angle_y_fc", shape: [1, 1] - Estimated yaw (in degrees).
+    name: "angle_p_fc", shape: [1, 1] - Estimated pitch (in degrees).
+    name: "angle_r_fc", shape: [1, 1] - Estimated roll (in degrees).
+
+        '''
+        #return self.outputs['angle_p_fc'][0,0], self.outputs['angle_r_fc'][0,0], self.outputs['angle_y_fc'][0,0]
+        return None
+
+class FaceLandmarks(GenericModel):
+    def __init__(self, model_path=FACE_LANDMARKS_MODEL):
+        super().__init__()
+        self.load_model(model_path)
+    
+    def preprocess_output(self):
+        '''
+        The net outputs a blob with the shape: [1, 10], containing a row-vector of 10 
+        floating point values for five landmarks coordinates in the form (x0, y0, x1, y1, ..., x5, y5). 
+        All the coordinates are normalized to be in range [0,1].
+        '''
+
+        return self.outputs[0,:,0,0]
+
+
+######
+# import cv2
+# img = cv2.imread('imagen.jpg')
+
+# facedetector = FaceDetector()
+# facedetector.predict(img)
+# facedetector.wait()
+# facedetector.get_output()
+# print('facedet',facedetector.preprocess_output())
+
+# headpose = HeadPose()
+# headpose.predict(img)
+# headpose.wait()
+# headpose.get_output()
+# print('headpose',headpose.outputs)
+
+# facelm = FaceLandmarks()
+# pred = facelm.predict(img)
+# facelm.wait()
+# facelm.get_output()
+# print('facelm',facelm.preprocess_output())
